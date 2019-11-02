cef0000d6dd2728cd9d4846c89e02803ef80daf8
Waseef Akhtar <waseef@degoo.com>
2019-11-03 00:18:18 +0500

    Adding import statements and cosnts

--- textify-app/app/src/main/java/com/waseefakhtar/myapplication/MainActivity.kt
+++ textify-app/app/src/main/java/com/waseefakhtar/myapplication/MainActivity.kt
@@ -2,6 +2,23 @@ package com.waseefakhtar.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.Manifest
+import android.content.pm.PackageManager
+import android.util.Size
+import android.graphics.Matrix
+import android.view.TextureView
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import java.util.concurrent.TimeUnit
+
+// This is an arbitrary number we are using to keep track of the permission
+// request. Where an app has multiple context for requesting permission,
+// this can help differentiate the different contexts.
+private const val REQUEST_CODE_PERMISSIONS = 10
+
+// This is an array of all the permission specified in the manifest.
+private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
 
 class MainActivity : AppCompatActivity() {
 
